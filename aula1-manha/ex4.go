91832e79fdada24ff14ddcbc6c7f770395a4deaf
Douglas Leonardo Mendes <doumendes@br0c02fq3krmd6m.ml.com>
2022-05-19 11:48:23 -0300

    refatoração dos arquivos e adicionado ex4

---
+++ aula1-manha/ex4.go
@@ -0,0 +1,35 @@
+/*
+Um estudante de programação tentou fazer declarações de variáveis com seus respectivos
+tipos em Go mas teve várias dúvidas ao fazê-lo. A partir disso, ele nos deu seu código e
+pediu a ajuda de um desenvolvedor experiente que pode:
+● Revisar o código e realizar as devidas correções.
+
+var sobrenome string = "Silva"
+var idade int = "25"
+boolean := "false";
+var salario string = 4585.90
+var nome string = "Fellipe"
+
+
+
+CORREÇÕES :
+
+PRIMEIRA OPÇÃO
+
+sobrenome := "Silva"
+idade := 25
+boolean := false
+salario := 4585.90
+nome := "Fellipe
+
+SEGUNDA OPÇÃO
+
+var (
+	sobrenome = "Silva"
+	idade := 25
+	boolean := false
+	salario := 4585.90
+	nome := "Fellipe
+)
+
+*/
