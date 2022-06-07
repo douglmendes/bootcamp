201e4d054f2975aaf034ea071b8ce86128fc0b32
Douglas L. Mendes <douglas.dmendes@mercadolivre.com>
2022-06-07 20:46:22 -0300

    add: refactoring code and tests

--- estruturas-metodos-interfaces/formas_test.go
+++ estruturas-metodos-interfaces/formas_test.go
@@ -3,10 +3,29 @@ package main
 import "testing"
 
 func TestPerimetro(t *testing.T) {
-	resultado := Perimetro(10.0, 10.0)
+	retangulo := Retangulo{10.0, 10.0}
+	resultado := Perimetro(retangulo)
 	esperado := 40.0
 
 	if resultado != esperado {
 		t.Errorf("resultado %.2f esperado %.2f", resultado, esperado)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	testesArea := []struct {
+		forma    Forma
+		esperado float64
+	}{
+		{forma: Retangulo{Largura: 12.0, Altura: 6.0}, esperado: 72.0},
+		{forma: Circulo{Raio: 10}, esperado: 314.1592653589793},
+		{forma: Triangulo{Base: 12, Altura: 6}, esperado: 36.0},
+	}
+	for _, tt := range testesArea {
+		resultado := tt.forma.Area()
+		if resultado != tt.esperado {
+			t.Errorf("%#vresultado %.2f, esperado %.2f", tt.forma, resultado, tt.esperado)
+		}
 	}
 }
